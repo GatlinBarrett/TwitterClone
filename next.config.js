50fdaf307980aa8c4861c32c3a6d48a7245f022b
Gatlin <wwgdb18@gmail.com>
2024-12-13 23:42:28 -0600

    twitter clone

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'rb.gy',
+          port: '',
+          pathname: '/ogau5a/**',
+          search: '',
+        },
+      ],
+    },
+  }
+
+//   https://rb.gy/ogau5a
